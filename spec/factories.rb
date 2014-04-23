39151c3dd130ed9f2262d346a4e5925754ec8605
Calhopp <calhopp@gmail.com>
2014-04-23 11:35:10 -0700

    Finish user edit, update, index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name	"Caleb Hopper"
-		email	"calhopp@gmail.com"
-		password "dota2mc"
-		password_confirmation "dota2mc"
-	end
+		sequence(:name)  { |n| "Person #{n}" }
+    	sequence(:email) { |n| "person_#{n}@example.com"}
+   	 	password "foobar"
+    	password_confirmation "foobar"
+
+    	factory :admin do
+    		admin true
+    	end
+    end
 end
